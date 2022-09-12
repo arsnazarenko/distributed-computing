18da9b1fa795facac6f3a8ac58f91c9864894972
arsnazarenko <arsnazarenko@gmail.com>
2022-09-12 19:40:43 +0300

    feat(lab1): add pipes

--- lab1/src/main.c
+++ lab1/src/main.c
@@ -6,12 +6,46 @@
 #include <sys/wait.h>
 #include "ipc.h"
 
+typedef struct {
+    int fd_int;
+    int fd_out;
+} node_pipes;
+
+
+
 int main(int argc, char **argv) {
+
+    int pipes[11][11][2] = {0};
+
+    for (size_t i = 0; i < 11; ++i) {
+        for (size_t j = 0; j < 11; ++j) {
+            if (i == j) { continue; }
+            if (pipe(pipes[i][j]) != 0) {
+                perror("pipe() failed");
+                break;
+            }
+            printf("%zu.%zu) fds = {%d, %d}\n", i, j, pipes[i][j][0], pipes[i][j][0]);
+        }
+    }
+
+    for (size_t i = 0; i < 11; ++i) {
+        for (size_t j = 0; j < 11; ++j) {
+            if (i == j) { continue; }
+            if (pipes[i][j][0] != 0) {
+                close(pipes[i][j][0]);
+            }
+            if (pipes[i][j][1] != 0) {
+                close(pipes[i][j][1]);
+            }
+        }
+    }
+
     /**
      * Duplex pipes between parent and child:
      * https://stackoverflow.com/questions/60104813/using-two-pipes-to-communicate-between-parent-process-and-child-process
      *
      */
+
     if (argc != 3) {
         fputs("Wrong number of arguments\n", stderr);
         return -1;
@@ -29,7 +63,7 @@ int main(int argc, char **argv) {
     for (size_t i = 0; i < n_proc; ++i, ++proc_id) {
         pid_t pid = fork();
         if (pid == -1) {
-            perror("fork");
+            perror("fork() failed");
             exit(1);
         } else if (pid == 0) {
             // child
