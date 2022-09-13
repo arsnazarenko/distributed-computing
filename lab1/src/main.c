af1fdb94966b07b7b3c75ee5a7b894535cc58527
arsnazarenko <arsnazarenko@gmail.com>
2022-09-13 03:37:16 +0300

    feat(lab1): changes in main.c

--- lab1/src/main.c
+++ lab1/src/main.c
@@ -4,47 +4,78 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <assert.h>
 #include "ipc.h"
+#include "common.h"
+#include "pa1.h"
+
+/**
+ * Duplex pipes between parent and child:
+ * https://stackoverflow.com/questions/60104813/using-two-pipes-to-communicate-between-parent-process-and-child-process
+ *
+ */
+
+#define MAX_PROC_COUNT 11
+
+typedef union {
+    int fds[2];
+    struct {
+        int fd_in;
+        int fd_out;
+    };
+} half_duplex_pipe;
 
 typedef struct {
-    int fd_int;
+    half_duplex_pipe input_pipe;
+    half_duplex_pipe output_pipe;
+} duplex_pipe;
+
+typedef struct {
+    int fd_in;
     int fd_out;
-} node_pipes;
+} node_interface;
+
+typedef struct {
+    local_id id;
+    node_interface adjacent[11];
+} node;
 
 
+static void close_pipe(half_duplex_pipe pipe) {
+    if (pipe.fd_in > 2) { close(pipe.fd_in); }
+    if (pipe.fd_out > 2) { close(pipe.fd_out); }
+}
+
+static void clear_pipe_table(duplex_pipe **pipe_table, const uint16_t sz) {
+    assert(sz > 0);
+    for (size_t i = 0; i < sz + 1; ++i) {
+        for (size_t j = 0; j < sz; ++j) {
+            if (i == j) { continue; }
+            close_pipe(pipe_table[i][j].input_pipe);
+            close_pipe(pipe_table[i][j].output_pipe);
+        }
+    }
+}
+
+static void init_pipe_table(duplex_pipe **pipe_table, const uint16_t sz) {
+    assert(sz > 0);
+    for (uint16_t i = 0; i < sz + 1; ++i) {
+        for (size_t j = 0; j < sz; ++j) {
+            if (i == j) { continue; }
+            half_duplex_pipe input_p = {0};
+            half_duplex_pipe output_p = {0};
+            if (pipe(input_p.fds) != 0 || pipe(output_p.fds) != 0) {
+                perror("pipe() failed");
+                clear_pipe_table(pipe_table, sz);
+                exit(1);
+            }
+            pipe_table[i][j] = (duplex_pipe) {input_p, output_p};
+            pipe_table[j][i] = (duplex_pipe) {output_p, input_p};
+        }
+    }
+}
 
 int main(int argc, char **argv) {
-
-    int pipes[11][11][2] = {0};
-
-    for (size_t i = 0; i < 11; ++i) {
-        for (size_t j = 0; j < 11; ++j) {
-            if (i == j) { continue; }
-            if (pipe(pipes[i][j]) != 0) {
-                perror("pipe() failed");
-                break;
-            }
-            printf("%zu.%zu) fds = {%d, %d}\n", i, j, pipes[i][j][0], pipes[i][j][0]);
-        }
-    }
-
-    for (size_t i = 0; i < 11; ++i) {
-        for (size_t j = 0; j < 11; ++j) {
-            if (i == j) { continue; }
-            if (pipes[i][j][0] != 0) {
-                close(pipes[i][j][0]);
-            }
-            if (pipes[i][j][1] != 0) {
-                close(pipes[i][j][1]);
-            }
-        }
-    }
-
-    /**
-     * Duplex pipes between parent and child:
-     * https://stackoverflow.com/questions/60104813/using-two-pipes-to-communicate-between-parent-process-and-child-process
-     *
-     */
 
     if (argc != 3) {
         fputs("Wrong number of arguments\n", stderr);
@@ -59,16 +90,25 @@ int main(int argc, char **argv) {
         fputs("Invalid value of option \"-p\"", stderr);
         return -1;
     }
+    const duplex_pipe pipes[MAX_PROC_COUNT][MAX_PROC_COUNT] = {0};
+    init_pipe_table((duplex_pipe **) pipes, n_proc);
+
     local_id proc_id = 1;
-    for (size_t i = 0; i < n_proc; ++i, ++proc_id) {
+    for (uint16_t i = 0; i < n_proc; ++i, ++proc_id) {
         pid_t pid = fork();
         if (pid == -1) {
             perror("fork() failed");
+            clear_pipe_table((duplex_pipe **) pipes, n_proc);
             exit(1);
         } else if (pid == 0) {
             // child
             printf("CHILD: pid = %d, local_id = %d\n", getpid(), proc_id);
-            // fixme: some_child_function();
+            duplex_pipe *adjacent = (duplex_pipe *) pipes[proc_id];
+            for (uint16_t k = 0; k < n_proc + 1; ++k) {
+                if (k == proc_id) { continue; }
+                close(adjacent[k].input_pipe.fd_in);
+                close(adjacent[k].output_pipe.fd_out);
+            }
             exit(0);
         }
     }
@@ -83,4 +123,5 @@ int main(int argc, char **argv) {
     }
     return 0;
 }
+
 
