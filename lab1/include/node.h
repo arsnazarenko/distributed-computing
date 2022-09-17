5c71a2f7178e0c7eb9b8a2b46e8cd6fd96cb79e0
arsnazarenko <arsnazarenko@gmail.com>
2022-09-17 04:44:46 +0300

    fix(lab1): changes in node struct implementation

--- lab1/include/node.h
+++ lab1/include/node.h
@@ -1,27 +1,16 @@
 #ifndef LAB1_NODE_H
 #define LAB1_NODE_H
+
+#include "inttypes.h"
 #include "context.h"
 
 typedef struct {
-    int fd_read;
-    int fd_write;
-} node_interface;
-
-typedef struct {
-    size_t sz;
-    node_interface interfaces[11];
-} adjacent_list;
-
-typedef struct {
-    uint8_t id;
+    int8_t id;
     adjacent_list neighbours;
 } node;
 
+void node_create(node *node, int8_t id, context *ctx);
 
-void node_create(node* node, uint8_t id, context* ctx);
-
-void node_destroy(node* node);
-
-
+void node_destroy(node *node);
 
 #endif //LAB1_NODE_H
--- lab1/src/node.c
+++ lab1/src/node.c
@@ -1,37 +1,13 @@
 #include <assert.h>
 #include <unistd.h>
-#include <stdio.h>
 #include "node.h"
 
-//static void node_debug(const node *node) {
-//    printf("{\n"
-//           "id: %d,\n"
-//           "{ sz: %zu { ", node->id, node->neighbours.sz);
-//    for (size_t i = 0; i < node->neighbours.sz; ++i) {
-//        printf("(%d, %d) ",
-//               node->neighbours.interfaces[i].fd_read,
-//               node->neighbours.interfaces[i].fd_write);
-//    }
-//    printf("}\n}\n");
-//}
-
-void node_create(node *node, uint8_t id, context *ctx) {
+void node_create(node *node, int8_t id, context *ctx) {
     assert(node != NULL && ctx != NULL);
-    assert(id >= 0 && id < 11);
-    size_t row = (size_t) id;
+    assert(id >= 0 && id < MAX_PROC_SIZE);
     node->id = id;
-    node->neighbours.sz = ctx->sz;
-    for (size_t i = 0; i < ctx->sz; ++i) {
-        // copy needed fd's to communicate for each processes in system
-        node->neighbours.interfaces[i] = (node_interface)
-                {.fd_read = ctx->pipe_table[row][i].input_pipe.fd_out,
-                        .fd_write =  ctx->pipe_table[row][i].output_pipe.fd_in};
-        // clean copied fd's from global table to prevent them from closing in context_destroy()
-        ctx->pipe_table[row][i].input_pipe.fd_out = -1;
-        ctx->pipe_table[row][i].output_pipe.fd_in = -1;
-        ctx->pipe_table[i][row].output_pipe.fd_out = -1;
-        ctx->pipe_table[i][row].input_pipe.fd_in = -1;
-    }
+    context_create_adjacent_list(ctx, id, &(node->neighbours));
+
 }
 
 void node_destroy(node *node) {
