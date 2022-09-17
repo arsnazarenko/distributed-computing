04c06c15b35f0f8bfdb1b3c8da383d35a58987e1
arsnazarenko <arsnazarenko@gmail.com>
2022-09-17 04:47:34 +0300

    feat(lab1): add main function implementation with correctly processes start

--- lab1/src/main.c
+++ lab1/src/main.c
@@ -8,18 +8,62 @@
 #include "ipc.h"
 #include "pa1.h"
 
-/**
- * Duplex pipes between parent and child:
- * https://stackoverflow.com/questions/60104813/using-two-pipes-to-communicate-between-parent-process-and-child-process
- *
- */
+//static void context_debug(const context *ctx) {
+//    printf("Context: sz: %zu\n", ctx->sz);
+//    for (size_t i = 0; i < ctx->sz; ++i) {
+//        for (size_t j = 0; j < ctx->sz; ++j) {
+//            printf("[%3d,%3d,%3d,%3d] ",
+//                   ctx->pipe_table[i][j].input_pipe.fd_read,
+//                   ctx->pipe_table[i][j].input_pipe.fd_write,
+//                   ctx->pipe_table[i][j].output_pipe.fd_read,
+//                   ctx->pipe_table[i][j].output_pipe.fd_write);
+//        }
+//        printf("\n");
+//    }
+//}
+//
+//static void node_debug(const node *node) {
+//    printf("id: %d, list: %zu, { ", node->id, node->neighbours.sz);
+//    for (size_t i = 0; i < node->neighbours.sz; ++i) {
+//        printf("(%d, %d) ",
+//               node->neighbours.interfaces[i].fd_read,
+//               node->neighbours.interfaces[i].fd_write);
+//    }
+//    printf("}\n");
+//}
+
+
+void work(node *node) {
+    const local_id max_id = (local_id) (node->neighbours.sz - 1);
+    local_id writer_id = 1;
+    while (writer_id <= max_id) {
+        if (node->id == writer_id) {
+            Message msg = {0};
+            sprintf(msg.s_payload, log_started_fmt, node->id, getpid(), getppid());
+            msg.s_header = (MessageHeader)
+                    {.s_type = STARTED,
+                            .s_payload_len = strlen(msg.s_payload),
+                            .s_local_time = 0,
+                            .s_magic = MESSAGE_MAGIC
+                    };
+            send_multicast(node, &msg);
+        } else {
+            Message msg = {0};
+            receive(node, writer_id, &msg);
+            msg.s_payload[msg.s_header.s_payload_len] = 0; // add null-terminator for reading payload
+            printf("Process %d (%d) received: %s", node->id, getpid(), msg.s_payload);
+        }
+        ++writer_id;
+    }
+}
+
 
 int main(int argc, char **argv) {
     if (argc != 3) {
         fputs("Wrong number of arguments\n", stderr);
         return -1;
     }
-    const char * const option = argv[1];
+    const char *const option = argv[1];
     if (strcmp("-p", option) != 0) {
         fputs("Wrong option", stderr);
     }
@@ -42,20 +86,22 @@ int main(int argc, char **argv) {
             context_destroy(&context);
             exit(1);
         } else if (pid == 0) {
-            printf("CHILD: pid = %d, local_id = %d\n", getpid(), proc_id);
+            // child
+//            printf("CHILD: pid = %d, local_id = %d\n", getpid(), proc_id);
             node node = {0};
             node_create(&node, proc_id, &context);
             context_destroy(&context);
-            // fixme: child work
+            work(&node);
             node_destroy(&node);
             exit(0);
         }
     }
-    printf("PARENT: pid = %d, local_id = %d\n", getpid(), 0);
-    node parent_node = {0};
-    node_create(&parent_node, 0, &context);
+//    printf("PARENT: pid = %d, local_id = %d\n", getpid(), 0);
+    node node = {0};
+    node_create(&node, 0, &context);
     context_destroy(&context);
-    node_destroy(&parent_node);
+    work(&node);
+    node_destroy(&node);
     int status;
     pid_t pid;
     for (size_t i = 0; i < n_proc; ++i) {
@@ -65,5 +111,7 @@ int main(int argc, char **argv) {
     }
     return 0;
 }
+
+
 
 
