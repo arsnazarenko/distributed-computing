f076a129f716e1f40c52a9b9c4f04c9363b77946
arsnazarenko <arsnazarenko@gmail.com>
2022-09-17 04:46:04 +0300

    feat(lab1): add ipc.h functions implementation

--- lab1/src/ipc.c
+++ lab1/src/ipc.c
@@ -1,2 +1,45 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
 #include "ipc.h"
+#include "node.h"
 
+int send(void *self, local_id dst, const Message *msg) {
+    const node *self_node = (node *) self;
+    if (dst == self_node->id) { return -1; }
+    if (write(self_node->neighbours.interfaces[dst].fd_write,
+              msg, sizeof(MessageHeader) + strlen(msg->s_payload)) == -1) {
+        perror("write() failed");
+        return -1;
+    }
+    return 0;
+}
+
+int send_multicast(void *self, const Message *msg) {
+    const node *self_node = (node *) self;
+    const local_id max_id = (local_id) (self_node->neighbours.sz - 1);
+    for (local_id id = 0; id <= max_id; ++id) {
+        if (id == self_node->id) { continue; }
+        if (send(self, id, msg) != 0) {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+int receive(void *self, local_id from, Message *msg) {
+    const node *self_node = (node *) self;
+    if (from == self_node->id) { return -1; }
+    if (read(self_node->neighbours.interfaces[from].fd_read,
+             msg, MAX_MESSAGE_LEN) == -1) {
+        perror("read() failed");
+        return -1;
+    }
+    return 0;
+}
+
+int receive_any(void *self, Message *msg) {
+    (void) self;
+    (void) msg;
+    return -1;
+}
