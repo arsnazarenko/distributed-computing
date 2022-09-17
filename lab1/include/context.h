e1ae8faa9c8a03d0bd8a960b48481c92ba36030a
arsnazarenko <arsnazarenko@gmail.com>
2022-09-17 04:34:50 +0300

    feat(lab1): change fields name in half_duplex_pipe struct

--- lab1/include/context.h
+++ lab1/include/context.h
@@ -6,28 +6,39 @@
 
 #define MAX_PROC_SIZE 11
 
+
 typedef union {
     int fds[2];
     struct {
-        int fd_in;
-        int fd_out;
+        int fd_read;
+        int fd_write;
     };
 } half_duplex_pipe;
-
 
 typedef struct {
     half_duplex_pipe input_pipe;
     half_duplex_pipe output_pipe;
 } duplex_pipe;
 
-
-typedef struct {
+typedef struct context {
     size_t sz;
     duplex_pipe pipe_table[MAX_PROC_SIZE][MAX_PROC_SIZE];
 } context;
 
+typedef struct {
+    int fd_read;
+    int fd_write;
+} node_interface;
+
+typedef struct {
+    size_t sz;
+    node_interface interfaces[MAX_PROC_SIZE];
+} adjacent_list;
+
 bool context_create(context *ctx, size_t sz);
 
 void context_destroy(context *ctx);
+
+void context_create_adjacent_list(context* ctx, size_t row_number, adjacent_list* adj_list);
 
 #endif //LAB1_CONTEXT_H
