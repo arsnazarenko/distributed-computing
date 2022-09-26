8aae5e9fd900e630909a4dfc4bbc22fcf4cc7c0d
arsnazarenko <arsnazarenko@gmail.com>
2022-09-26 03:31:02 +0300

    fix(lab1): small fixes

--- lab1/src/main.c
+++ lab1/src/main.c
@@ -8,6 +8,7 @@
 #include "ipc.h"
 #include "pa1.h"
 
+#define _POSIX_C_SOURCE
 //static void context_debug(const context *ctx) {
 //    printf("Context: sz: %zu\n", ctx->sz);
 //    for (size_t i = 0; i < ctx->sz; ++i) {
@@ -33,8 +34,8 @@
 //}
 
 
-void work(node *node) {
-    const local_id max_id = (local_id) (node->neighbours.sz - 1);
+void work(node *node, size_t child_proc_number) {
+    const local_id max_id = (local_id) (child_proc_number);
     local_id writer_id = 1;
     while (writer_id <= max_id) {
         if (node->id == writer_id) {
@@ -42,16 +43,16 @@ void work(node *node) {
             sprintf(msg.s_payload, log_started_fmt, node->id, getpid(), getppid());
             msg.s_header = (MessageHeader)
                     {.s_type = STARTED,
-                            .s_payload_len = strlen(msg.s_payload),
-                            .s_local_time = 0,
-                            .s_magic = MESSAGE_MAGIC
+                     .s_payload_len = strlen(msg.s_payload),
+                     .s_local_time = 0,
+                     .s_magic = MESSAGE_MAGIC
                     };
             send_multicast(node, &msg);
         } else {
             Message msg = {0};
             receive(node, writer_id, &msg);
             msg.s_payload[msg.s_header.s_payload_len] = 0; // add null-terminator for reading payload
-            printf("Process %d (%d) received: %s", node->id, getpid(), msg.s_payload);
+            printf("Process %d (%d) received: %s\n", node->id, getpid(), msg.s_payload);
         }
         ++writer_id;
     }
@@ -65,20 +66,21 @@ int main(int argc, char **argv) {
     }
     const char *const option = argv[1];
     if (strcmp("-p", option) != 0) {
-        fputs("Wrong option", stderr);
+        fputs("Wrong option\n", stderr);
+        return -1;
     }
-    const size_t n_proc = strtol(argv[2], NULL, 10);
-    if (n_proc <= 0 || n_proc > 10) {
-        fputs("Invalid value of option \"-p\"", stderr);
+    const size_t child_proc_number = strtol(argv[2], NULL, 10);
+    if (child_proc_number < 1 || child_proc_number > 9) {
+        fputs("Invalid value of option \"-p\"\n", stderr);
         return -1;
     }
     context context = {0};
-    if (!context_create(&context, n_proc + 1)) {
+    if (context_create(&context, child_proc_number + 1) != 0) {
         context_destroy(&context);
         exit(1);
     }
-    local_id proc_id = 1;
-    for (size_t i = 0; i < n_proc; ++i, ++proc_id) {
+    local_id max_child_id = (local_id) child_proc_number;
+    for (local_id child_id = 1; child_id <= max_child_id; ++child_id) {
         pid_t pid = fork();
         if (pid == -1) {
             // error
@@ -87,24 +89,22 @@ int main(int argc, char **argv) {
             exit(1);
         } else if (pid == 0) {
             // child
-//            printf("CHILD: pid = %d, local_id = %d\n", getpid(), proc_id);
             node node = {0};
-            node_create(&node, proc_id, &context);
+            node_create(&node, child_id, &context);
             context_destroy(&context);
-            work(&node);
+            work(&node, child_proc_number);
             node_destroy(&node);
             exit(0);
         }
     }
-//    printf("PARENT: pid = %d, local_id = %d\n", getpid(), 0);
     node node = {0};
-    node_create(&node, 0, &context);
+    node_create(&node, PARENT_ID, &context);
     context_destroy(&context);
-    work(&node);
+    work(&node, child_proc_number);
     node_destroy(&node);
     int status;
     pid_t pid;
-    for (size_t i = 0; i < n_proc; ++i) {
+    for (size_t i = 0; i < child_proc_number; ++i) {
         printf("%zu. ", i + 1);
         pid = wait(&status);
         printf("Child with pid %d exit with status %d\n", pid, status);
