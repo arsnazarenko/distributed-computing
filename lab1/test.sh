83f65a6309c6e41e699c9daf088586d3de8eaded
arsnazarenko <arsnazarenko@gmail.com>
2022-11-13 01:36:09 +0300

    fix(lab1): fix Makefile

--- lab1/test.sh
+++ lab1/test.sh
@@ -3,7 +3,7 @@ for i in {1..9}
 do
   for j in {0..100}
   do
-    ERROR=$(./build/default/main -p $i 2>&1 1>/dev/null)
+    ERROR=$(./build/pa1 -p $i 2>&1 1>/dev/null)
     if [ $? -eq 0 ]
     then
       STARTED=$(cat ./events.log | grep "has STARTED$" | wc -l)
@@ -12,12 +12,12 @@ do
       RECEIVE_DONE=$(cat ./events.log | grep "DONE messages$" | wc -l)
       if [ $STARTED -eq $i ] && [ $RECEIVE_STARTED -eq $(( $i + 1 )) ] && [ $DONE -eq $i ] && [ $RECEIVE_DONE -eq $(( $i + 1 )) ];
       then
-        echo "./build/default/main -p $i: GOOD OUTPUT"
+        echo "./build/pa1 -p $i: GOOD OUTPUT"
       else
-        echo "./build/default/main -p $i: BAD OUTPUT"
+        echo "./build/pa1 -p $i: BAD OUTPUT"
       fi
     else
-      echo "./build/default/main -p $i: $ERROR"
+      echo "./build/pa1 -p $i: $ERROR"
     fi
   done
 done
